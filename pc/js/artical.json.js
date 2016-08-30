dfe5437402f3bb40670f0241299e624e4dee24a9
douchaoyang <douchaoyang@gmail.com>
2016-08-30 15:19:05 +0800

    将lottery添加至列表

--- pc/js/artical.json.js
+++ pc/js/artical.json.js
@@ -2,7 +2,12 @@ loadArtical({
 	"token":1,
 	"ret":[
 		{
-			"title":"Yplayer-PC端JavaScript原生视频播放器插件，向下兼容至IE6",
+			"title":"Lottery-JavaScript原生抽奖插件 &gt;IE6",
+			"url":"../sc/Lottery/",
+			"time":"2016-08-30"
+		},
+		{
+			"title":"Yplayer-PC端JavaScript原生视频播放器插件，&gt;IE6",
 			"url":"../sc/Yplayer/",
 			"time":"2016-08-30"
 		}
