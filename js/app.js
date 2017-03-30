bf9aa0ccd3a6bb7c26396a94acea0f447cb936ab
douchaoyang <douchaoyang@gmail.com>
2017-03-30 15:18:07 +0800

    alpha 20170330

---
+++ js/app.js
@@ -0,0 +1,10 @@
+(function(){
+	if(window.HTMLCanvasElement)
+	{
+		var _s = document.createElement("script");
+		var _h = document.getElementsByTagName("head")[0];
+		_s.type = "text/javascript";
+		_s.src = "js/particles.js";
+		_h.appendChild(_s);
+	}
+})();
