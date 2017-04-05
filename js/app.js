5dce3d583dd8e64729287bd78a98d7bdfd1f057c
douchaoyang <douchaoyang@gmail.com>
2017-04-05 16:42:10 +0800

    index mod

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+/*canvas*/
 (function(){
 	if(window.HTMLCanvasElement)
 	{
@@ -8,3 +9,12 @@
 		_h.appendChild(_s);
 	}
 })();
+
+/*163 music*/
+var SONG_ID = 32192436;
+function load163Music(id, song)
+{
+	var _html="<iframe frameborder=\"no\" border=\"0\" marginwidth=\"0\" marginheight=\"0\" width=1000 height=86 src=\"http://music.163.com/outchain/player?type=2&id="+song+"&auto=0&height=66\"></iframe>";
+	document.getElementById(id).innerHTML=_html
+};
+load163Music("music", SONG_ID);
